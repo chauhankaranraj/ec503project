26c9c3716c111bb1011e43916fd239a370bb7a39
Karanraj Chauhan <chauhank@bu.edu>
2017-11-17 17:35:57 -0500

    working opticsgit add .! but old version

--- optics_test.m
+++ optics_test.m
@@ -2,9 +2,15 @@ load fisheriris
 
 X = meas(:,3:4);
 
-[order, reach_dists, core_dists] = OPTICSv2(X, 0.5, 5);
+% [order, reach_dists, core_dists] = OPTICSv2(X, 0.5, 5);
 
-figure;
+[order, reach_dists] = optics(X, 0.5, 2);
+
+plot(order, reach_dists);
+title 'OPTICS on Fisher Iris'
+
+
+% figure;
 % 
 % for c_num = 1:max(k_preds)
 %     scatter3(X(k_preds==c_num,1), X(k_preds==c_num,2), X(k_preds==c_num,3));
