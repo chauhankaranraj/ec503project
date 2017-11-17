aab3b45a3593dd8bc6c45660a707d39e6ed992fb
Karanraj Chauhan <chauhank@bu.edu>
2017-11-17 15:09:57 -0500

    adding song dataset as datapoints

--- test.m
+++ test.m
@@ -1,12 +1,17 @@
-% testing out reader
-song = HDF5_Song_File_Reader('./sample_data/TRAAAAW128F429D538.h5');
+% % testing out reader
+% song = HDF5_Song_File_Reader('./sample_data/TRAAAAW128F429D538.h5');
+% 
+% % song attribuets
+% danceability = song.get_danceability()
+% duration = song.get_duration()
+% energy = song.get_energy()
+% hotness = song.get_song_hotttnesss()
+% title = song.get_title()
+% 
+% % what methods have they implemented??
+% methods('HDF5_Song_File_Reader')
 
-% song attribuets
-danceability = song.get_danceability()
-duration = song.get_duration()
-energy = song.get_energy()
-hotness = song.get_song_hotttnesss()
-title = song.get_title()
-
-% what methods have they implemented??
-methods('HDF5_Song_File_Reader')
+files = dir('./sample_data/*.txt');
+for i=1:length(files)
+    eval(['load ' files(i).name ' -ascii']);
+end
