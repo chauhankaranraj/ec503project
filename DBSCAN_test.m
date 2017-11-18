4032aece9d6cec15e08c6310baa18d7fef1e06de
Karanraj Chauhan <chauhank@bu.edu>
2017-11-17 20:16:12 -0500

    added dbscan graph

--- DBSCAN_test.m
+++ DBSCAN_test.m
@@ -1,9 +1,18 @@
 load fisheriris
 
 
-cluster1 = normrnd(-5, 1, 100, 1);
-cluster2 = normrnd(5, 1, 100, 1);
+% cluster1 = normrnd(-5, 1, 100, 1);
+% cluster2 = normrnd(5, 1, 100, 1);
+% 
+% X = [cluster1; cluster2];
 
-X = [cluster1; cluster2];
+X = meas(:,3:4);
 
-[assignments, li_noise] = DBSCAN(X, 0.5, 5, 'mahlanobis');
+[assignments, li_noise] = DBSCAN(X, 0.5, 5);
+
+figure;
+
+for c_num = 1:max(assignments)
+    scatter(X(assignments==c_num,1), X(assignments==c_num,2), );
+    hold on
+end
