448341248d79f27e03654a255de10f3473d9d115
Karanraj Chauhan <chauhank@bu.edu>
2017-11-17 20:16:28 -0500

    Added more dbscan graphs

--- DBSCAN_test.m
+++ DBSCAN_test.m
@@ -8,11 +8,13 @@ load fisheriris
 
 X = meas(:,3:4);
 
-[assignments, li_noise] = DBSCAN(X, 0.5, 5);
+[assignments, li_noise] = DBSCAN(X, 0.5, 3);
 
 figure;
 
 for c_num = 1:max(assignments)
-    scatter(X(assignments==c_num,1), X(assignments==c_num,2), );
+    scatter(X(assignments==c_num,1), X(assignments==c_num,2), 'filled');
     hold on
 end
+
+legend('Cluster1', 'Cluster2', 'Cluster3', 'Cluster4');
