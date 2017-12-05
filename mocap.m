78ea65f2e9ce51b5d82548e692cca9388ab88141
Karanraj Chauhan <chauhank@bu.edu>
2017-12-05 00:47:10 -0500

    ran kmeans on mocap and saved ccr vs k plot

--- mocap.m
+++ mocap.m
@@ -1,6 +1,46 @@
+%% LOAD
+
 % load dataset, skip first row of feature titles
 X = csvread('../preprocess/processed_mocap.csv', 1, 1);
 y = csvread('../preprocess/mocap_labels.csv', 0, 1);
 
-% run in built kmeans
-y_pred = kmeans(X, 5);
+% sort in increasing order of labels
+[y, sort_idx] = sort(y);
+X = X(sort_idx, :);
+
+% dataset shape
+num_pts = numel(y);
+num_feats = size(X, 2);
+
+
+%% CLUSTER
+
+% evaluation metrics for various k
+ccrs = zeros(10, 1);
+
+best_k = 0;
+best_ccr = 0;
+best_y_pred = zeros(num_pts,1);
+
+% cross validate for k
+for k = 1:10
+    
+    fprintf('k=%i\n', k);
+    
+    % run in-built kmeans
+    y_pred = kmeans(X, k);
+    
+    % evaluate and save metrics
+    ccrs(k) = sum(y==y_pred) / num_pts;
+    
+    % update best k, ccr, and corresponding predictions
+    if ccrs(k) > best_ccr
+        best_k = k;
+        best_ccr = ccrs(k);
+        best_y_pred = y_pred;
+    end
+
+end
+
+plot(1:10, ccrs);
+
--- scratchpad.m
+++ scratchpad.m
@@ -1,12 +1,14 @@
-level = 5;
-semester = 'Fall';
-subject = 'Math';
-student = 'John_Doe';
-fieldnames = {semester subject student};
+% level = 5;
+% semester = 'Fall';
+% subject = 'Math';
+% student = 'John_Doe';
+% fieldnames = {semester subject student};
+% 
+% % Add data to a structure named grades.
+% grades(level).(semester).(subject).(student)(10,21:30) = ...
+%              [85, 89, 76, 93, 85, 91, 68, 84, 95, 73];
+% 
+% % Retrieve the data added.
+% getfield(grades, {level}, fieldnames{:}, {10,21:30})
 
-% Add data to a structure named grades.
-grades(level).(semester).(subject).(student)(10,21:30) = ...
-             [85, 89, 76, 93, 85, 91, 68, 84, 95, 73];
-
-% Retrieve the data added.
-getfield(grades, {level}, fieldnames{:}, {10,21:30})
+foo = ones(1, 3, 5);
