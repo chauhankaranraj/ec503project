734847b65370fe88232f8fbcc7f6d43f0582702d
Karanraj Chauhan <chauhank@bu.edu>
2017-12-10 13:19:54 -0500

    modify and fix priority queue implementation

--- optics.m
+++ optics.m
@@ -36,22 +36,24 @@ function [ordered_idx_list, reach_dists] = optics(X, epsilon, min_pts)
         
         if ~isnan(get_core_dist(X(p_idx, :)))
             
-            % priority queue. value at idx is priority of point at idx
-            priorities = zeros(num_pts, 1) - 1;
+            % least-first priority queue. value at idx is priority of point at idx
+            priorities = Inf(num_pts, 1);
             
             % get the updated reachability distances and priority queue seeds
             update(X(p_idx,:), is_idx_neighbor);
             
             % sorts descending, gives indices of points with descending priorities
-            [priorities, pr_pt_idx] = sort(priorities, 'descend');
+            [priorities, pr_pt_idx] = sort(priorities);
             
             for pr_q_idx = 1:numel(priorities)
                 
                 % q_idx is index of point in dataset
+                % it is least priority element in seeds
                 q_idx = pr_pt_idx(pr_q_idx);
                 
-                % no more points with non-negative priority, so we're done
-                if priorities(q_idx)==-1
+                % no more points with finite priority, so we're done
+                % equivalent to seed list empty
+                if priorities(pr_q_idx)==Inf
                     break
                 end
                 
@@ -65,7 +67,7 @@ function [ordered_idx_list, reach_dists] = optics(X, epsilon, min_pts)
                 % add q to the ordered list by finding first nan and replacing it with q
                 ordered_idx_list(find(isnan(ordered_idx_list),1), 1) = q_idx;
                 
-                % if core dist of q is undefined then update
+                % if core dist of q is not undefined then update
                 if ~isnan(get_core_dist(X(q_idx, :)))
                    
                     % get the updated reachability distances and priority queue seeds
@@ -152,7 +154,7 @@ function [ordered_idx_list, reach_dists] = optics(X, epsilon, min_pts)
                     % update value of reachability distance
                     reach_dists(o_idx) = curr_reach_dist;
 
-                    % add to priority queue
+                    % update priority queue
                     priorities(o_idx) = curr_reach_dist;
 
                 end
@@ -165,91 +167,3 @@ function [ordered_idx_list, reach_dists] = optics(X, epsilon, min_pts)
     
     
 end
-
-
-% function is_neighbors = get_neighbors(p, X, epsilon)
-% % get_neighbors Gets the indices of points present in dataset input X that
-% % are in epsilon region of data point p, i.e., the neighbors of p
-% %
-% % Assumes each column of X (nxd) is dimension and each row is data point
-% %
-% % Returns indices of neighbors of p
-% 
-%     % distance of each point from p
-%     distances = pdist2(X, p, 'euclidean');
-%     
-%     % return indices of points which are in eps neighborhood of p
-%     is_neighbors = distances >= epsilon;
-% 
-% end
-% 
-% 
-% function core_dist = get_core_dist(p, X, epsilon, min_pts)
-% % get_core_dist Calculates the distance of each point in epsilon neighborhood
-% % of p and returns the min_pt-th closest point
-% %
-% % Assumes each column of X (nxd) is dimension and each row is data point
-% %
-% % Returns core distance
-% 
-%     % distance of each point from p
-%     distances = pdist2(X, p, 'euclidean');
-%     
-%     % indices of points which are in eps neighborhood of p
-%     neighbors_idx = distances >= epsilon;
-%     
-%     % core dist undefined if number of pts in eps neighborhood is < MinPts
-%     if sum(neighbors_idx) < min_pts
-%         core_dist = NaN;
-%     else
-%         % remove distances to points which are not in eps neighborhood
-%         distances = sort(distances(neighbors_idx, :));
-% 
-%         % get the min_pts-th closest distance
-%         core_dist = distances(min_pts, 1);
-%     end
-% 
-% end
-% 
-% 
-% function [new_reach_dists, new_priorities] = update(p, X, epsilon, min_pts, priorities, is_idx_neighbor, reach_dists)
-% % get_core_dist Calculates the distance of each point in epsilon neighborhood
-% % of p and returns the min_pt-th closest point
-% %
-% % Assumes each column of X (nxd) is dimension and each row is data point
-% %
-% % Returns core distance
-% 
-%     % number of points in dataset
-%     num_pts = size(X,1);
-% 
-%     % initialize as same values as original reachability dists and seeds
-%     new_reach_dists = reach_dists;
-%     new_priorities = priorities;
-% 
-%     % get core distance of p
-%     core_dist = get_core_dist(p, X, epsilon, min_pts);
-%     
-%     for o_idx = 1:num_pts
-%         
-%         % if point is a neighbor that has not been processed
-%         if is_idx_neighbor(o_idx) && ~is_idx_processed(o_idx)
-%         
-%             % calculate new reachability dist as max of core dist and dist(p,o)
-%             curr_reach_dist = max(core_dist, pdist2(p, X(o_idx, :)));
-% 
-%             if (isnan(reach_dists(o_idx))) || (curr_reach_dist < reach_dists(o_idx))
-% 
-%                 % update value of reachability distance
-%                 new_reach_dists(o_idx) = curr_reach_dist;
-%                 
-%                 % add to priority queue
-%                 new_priorities(o_idx) = curr_reach_dist;
-% 
-%             end
-%     
-%         end
-%         
-%     end
-% 
-% end
