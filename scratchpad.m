09cf8bfae0d674b7b93c607edd2977f9022e37ac
Karanraj Chauhan <chauhank@bu.edu>
2017-12-11 06:44:46 -0500

    Added reachability plots

--- scratchpad.m
+++ scratchpad.m
@@ -11,4 +11,30 @@
 % % Retrieve the data added.
 % getfield(grades, {level}, fieldnames{:}, {10,21:30})
 
-foo = ones(1, 3, 5);
+y = zeros(18,1);
+
+y(1) = 10;
+% y(2) = 2;
+% y(3) = 1.5;
+% y(4) = 1.25;
+% y(5) = 1;
+% y(6) = 0.5;
+% y(7) = 0.4;
+% y(8) = 0.5;
+% y(9) = 1;
+% y(10) = 2;
+% y(11) = 1.75;
+% y(12) = 1.5;
+% y(13) = 1.5;
+% y(14) = 1.25;
+% y(15) = 1.75;
+% y(16) = 4;
+% y(17) = 2;
+% y(18) = 2;
+
+stem(1:18, y, 'filled');
+xlabel 'Point'
+ylabel 'Reachability Distance'
+title 'Reachability Plot'
+
+
