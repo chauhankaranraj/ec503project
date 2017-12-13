8827f6673659ff66363ca1cbf2aa36278011bdb5
Karanraj Chauhan <chauhank@bu.edu>
2017-12-13 12:43:30 -0500

    added breast cancer dataset and test file

---
+++ test_all_bc.m
@@ -0,0 +1,97 @@
+%% LOAD DATA
+
+fileID = fopen('./bc/wdbc.data');
+X = textscan(fileID,'%d, %c, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f');
+fclose(fileID);
+
+% split x, y, id num
+ids = cell2mat(X(1, 1));
+y = cell2mat(X(1, 2));
+X = cell2mat(X(1, 3:end));
+
+% convert char to double. 1 is malignant, 2 is benign
+y = grp2idx(y);
+
+num_pts = size(X,1);
+num_features = size(X,2);
+
+%% K, K++ PARAMETERS
+
+% only two classes - m, b
+k = 2;
+
+% number of iterations to run in k, k++, dp
+num_iters = 100;
+
+
+%% K-MEANS
+
+% get assignments and within cluster sum of squares
+[k_assignments, k_centroids, k_wcss] = k_means(X, k, num_iters);
+
+% nmi score
+k_nmi = nmi(k_assignments, y);
+
+
+%% K-MEANS++
+
+% get assignments and within cluster sum of squares
+[kpp_assignments, kpp_centroids, kpp_wcss] = k_means_pp(X, k, num_iters);
+
+% nmi score
+kpp_nmi = nmi(kpp_assignments, y);
+
+
+%% DP-MEANS
+
+% cost to start new cluster in dp
+lambda = 10000000;
+
+% get assignments and within cluster sum of squares
+[dp_assignments, dp_k, dp_objfunc, dp_centroids] = DPMeans(X, lambda);
+
+% nmi score
+dp_nmi = nmi(dp_assignments, y);
+
+
+%% DBSCAN
+
+% init dbscan parameters
+eps = 10;
+min_pts = 4;
+
+% % CROSS VALIDATE FOR PARAMETERS
+% 
+% % distance matrix
+% DM = pdist2(X, X);
+% 
+% k_dists = zeros(num_pts, 1);
+% for pt_num = 1:num_pts
+%     dists = DM(pt_num, :);
+%     dists = sort(dists, 'ascend');
+%     k_dists(pt_num) = dists(pt_num);
+% end
+% k_dists = sort(k_dists, 'descend');
+% 
+% % knee plot
+% figure;
+% plot(1:num_pts, k_dists);
+% xlabel 'Point'
+% ylabel 'K-Dist'
+% title 'Distance to k-th NN for MinPts = 4'
+
+nmis = zeros
+figure;
+for i = 10:10:100
+    % get assignments and noise labels
+    [dbscan_assignments, dbscan_noise] = DBSCAN(X, 60, 4);
+
+    % nmi score
+    dbscan_nmi = nmi(dbscan_assignments, y);
+    
+    plot(i, dbscan_nmi);
+    hold on;
+end
+hold off;
+
+
